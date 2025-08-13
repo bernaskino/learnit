5b1fc7e885ffc451f94d60b9ca2650bd98eb5a0d
Marcos Bernasconi <xb3rna@gmail.com>
2025-08-13 12:04:45 +0200

    style: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="h-dvh w-dvw overflow-clip">{children}</main>
       </body>
     </html>
   );
